ee04865073a52b7c4b423c7682eb82c948b17309
ibizaSunrise <milalemekh@gmail.com>
2020-11-26 15:51:50 +0100

    Added form component

--- src/containers/faqs.js
+++ src/containers/faqs.js
@@ -1,24 +1,26 @@
 import React from 'react'
 import { Accordion } from '../components'
+import OptForm from '../components/opt-form'
 import faqsData from '../fixtures/faqs'
 
-export function FaqsContainer(){
+export function FaqsContainer() {
     return (
         <Accordion>
             <Accordion.Title>Frequently Asked Questions</Accordion.Title>
-            {faqsData.map(item =>
-                <Accordion.Item key={item.id}>
-                    <Accordion.Header>
-                        {item.header}
-                    </Accordion.Header>
-                    <Accordion.Body>
-                        {item.body}
-                    </Accordion.Body>
-                </Accordion.Item>
-                )}
             <Accordion.Frame>
-
+                {faqsData.map((item) => (
+                    <Accordion.Item key={item.id}>
+                        <Accordion.Header>{item.header}</Accordion.Header>
+                        <Accordion.Body>{item.body}</Accordion.Body>
+                    </Accordion.Item>
+                ))}
             </Accordion.Frame>
+            <OptForm>
+                <OptForm.Text>Redy to watch? Enter your email to create or restart your membership</OptForm.Text>
+                <OptForm.Break />
+                <OptForm.Input placecholder="Email address" />
+                <OptForm.Button>Try it now</OptForm.Button>
+            </OptForm>
         </Accordion>
     )
 }
