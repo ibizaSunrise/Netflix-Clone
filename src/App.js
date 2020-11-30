74a3c41a796740cbdc930c6eeb30a99964f25f68
ibizaSunrise <milalemekh@gmail.com>
2020-11-30 10:52:04 +0100

    Added helpers: routes(React router auth)

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import * as ROUTES from './constants/routes';
-import { Home, Browse, Signin, Signup} from './pages'
+import { Home, Browse, Signin, Signup } from './pages';
+import { IsUserRedirect } from './helpers/routes'
 
 export default function App() {
+  const user = {};
   return (
     <Router>
+        <IsUserRedirect user = {user} loggedInPath = {ROUTES.BROWSE} path={ROUTES.SIGN_IN} exact>
+        <Signin/>
+        </IsUserRedirect>
+
+        <IsUserRedirect user = {user} loggedInPath = {ROUTES.BROWSE} path={ROUTES.SIGN_UP} exact>
+        <Signup/>
+        </IsUserRedirect>
+
       <Route exact path='/browse'>
-    <Browse />
-  </Route>
-      <Route exact path='/signin'>
-    <Signin />
-  </Route>
-      <Route exact path='/signup'>
-    <Signup />
-  </Route>
+        <Browse />
+      </Route>
       <Route exact path={ROUTES.HOME}>
         <Home />
       </Route>
