748cee54d23079cc79afda756564644b2301a403
ibizaSunrise <milalemekh@gmail.com>
2020-12-01 11:42:30 +0100

    Added browse page(categories films & series component), custom hook useContent

--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,6 +1,15 @@
-import React from 'react'
+import React from 'react';
+import {useContent} from '../hooks'
 
 export default function Browse() {
+    // we need the series and the films
+    const {series} = useContent('series');
+    const {films} = useContent('films');
+
+    //we need slides
+    //pass it to the browse container
+    
+   
     return (
         <div>
             Hello from the browse!
