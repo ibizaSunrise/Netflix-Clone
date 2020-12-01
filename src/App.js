144f42652987ddc22d890e74bca65f709acb75c7
ibizaSunrise <milalemekh@gmail.com>
2020-12-01 10:41:33 +0100

    Added auth listener(custom hook)

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { BrowserRouter as Router, Switch } from 'react-router-dom';
 import * as ROUTES from './constants/routes';
 import { Home, Browse, Signin, Signup } from './pages';
-import { IsUserRedirect, ProtectedRoute } from './helpers/routes'
+import { IsUserRedirect, ProtectedRoute } from './helpers/routes';
+import {useAuthListener} from './hooks'
 
 export default function App() {
-  const user = null;
+  const {user} = useAuthListener();
+  console.log(user);
   return (
     <Router>
       <Switch>
