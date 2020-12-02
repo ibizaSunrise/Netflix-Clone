5cff652e38cdbc7964116dfa68a33a25e3d7cf93
ibizaSunrise <milalemekh@gmail.com>
2020-12-02 14:18:10 +0100

    Added utils directory

---
+++ src/containers/browse.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function BrowseContainer({slidex}) {
+    return (
+        <div>
+            hi
+        </div>
+    )
+}
--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,18 +1,15 @@
 import React from 'react';
-import {useContent} from '../hooks'
+import BrowseContainer from '../containers/browse';
+import {useContent} from '../hooks';
+import selectionFilter from '../utils/selection-filter';
 
 export default function Browse() {
-    // we need the series and the films
+    
     const {series} = useContent('series');
     const {films} = useContent('films');
-
-    //we need slides
-    //pass it to the browse container
-    
+    const slides = selectionFilter({series, films});
    
     return (
-        <div>
-            Hello from the browse!
-        </div>
+        <BrowseContainer slides={slides}/>
     )
 }
