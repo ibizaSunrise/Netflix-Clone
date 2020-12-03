f489f80b282c6bcf9049493012fc785da7bd4680
ibizaSunrise <milalemekh@gmail.com>
2020-12-03 10:39:17 +0100

    Added loading component

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -1,20 +1,26 @@
-import React, {useState, useContext, useEffect} from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import { SelectProfileContainer } from './profiles';
-import {FirebaseContext} from '../context/firebase';
-import {Loading} from '../components'
+import { FirebaseContext } from '../context/firebase';
+import { Header, Loading } from '../components'
 
-export default function BrowseContainer({slidex}) {
+export default function BrowseContainer({ slidex }) {
     const [profile, setProfile] = useState({});
     const [loading, setLoading] = useState(true);
     const { firebase } = useContext(FirebaseContext);
     const user = firebase.auth().currentUser || {};
 
-    useEffect (()=>{
-        setTimeout(()=>{
+    useEffect(() => {
+        setTimeout(() => {
             setLoading(false);
         }, 3000)
     }, [profile.displayName]);
+
     return profile.displayName ? (
-        loading ? <Loading src={user.photoURL}/> : null
-    ) : <SelectProfileContainer user={user} setProfile = {setProfile}/>
+        <>
+            {loading ? <Loading src={user.photoURL} /> : <Loading.ReleaseBody />}
+            <Header src="joker1"> Hello!</Header>
+        </>
+    ) : (
+            <SelectProfileContainer user={user} setProfile={setProfile} />
+        )
 }
