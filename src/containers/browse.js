7328c550d91afe52271e20e26c07a2feacb14716
ibizaSunrise <milalemekh@gmail.com>
2020-12-05 09:50:01 +0100

    Added card list(categories, slides) - film & series logic

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -1,14 +1,17 @@
 import React, { useState, useContext, useEffect } from 'react';
 import { SelectProfileContainer } from './profiles';
 import { FirebaseContext } from '../context/firebase';
-import { Header, Loading } from '../components';
+import { Card, Header, Loading } from '../components';
 import * as ROUTES from '../constants/routes';
 import logo from '../logo.svg'
 
-export default function BrowseContainer({ slidex }) {
+
+export default function BrowseContainer({ slides }) {
+    const [category, setCategory] = useState('series');
     const [searchTerm, setSearchTerm] = useState('');
     const [profile, setProfile] = useState({});
     const [loading, setLoading] = useState(true);
+    const [slideRows, setSlideRows] = useState([]);
     const { firebase } = useContext(FirebaseContext);
     const user = firebase.auth().currentUser || {};
 
@@ -17,6 +20,10 @@ export default function BrowseContainer({ slidex }) {
             setLoading(false);
         }, 3000)
     }, [profile.displayName]);
+
+    useEffect(()=>{
+        setSlideRows(slides[category]);
+    }, [slides, category]);
 
     return profile.displayName ? (
         <>
@@ -27,8 +34,8 @@ export default function BrowseContainer({ slidex }) {
                 <Header.Frame>
                     <Header.Group>
                     <Header.Logo to={ROUTES.HOME} src={logo} alt="Natflix"/>
-                    <Header.TextLink>Series</Header.TextLink>
-                    <Header.TextLink>Films</Header.TextLink>
+                    <Header.TextLink active={category === 'series' ? 'true': 'false'} onClick = {()=>setCategory('series')}>Series</Header.TextLink>
+                    <Header.TextLink active={category === 'films' ? 'true': 'false'} onClick = {()=>setCategory('films')}>Films</Header.TextLink>
                     </Header.Group>
                     <Header.Group>
                         <Header.Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
@@ -58,6 +65,13 @@ export default function BrowseContainer({ slidex }) {
                 </Header.Feature>
             </Header>
             
+            <Card.Group>
+                {slideRows.map((slideItem)=>(
+                    <Card key={`${category}-${slideItem.title.toLowerCase()}`}>
+                        <Card.Title>{slideItem.title}</Card.Title>
+                    </Card>
+                ))}
+            </Card.Group>
         </>
     ) : (
             <SelectProfileContainer user={user} setProfile={setProfile} />
